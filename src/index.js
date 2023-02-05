d2e6e1ad1be1aeacc51b5a5b17c3c173a21ae3e9
OlegPetin <olegpetin87@gmail.com>
2023-02-05 21:30:15 +0300

    feat: edit js make OPEN_BRACKETS and BRACKETS_PAIR arrays

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const OPEN_BRACKETS = [];
+  const BRACKETS_PAIR = {};
+  let k = bracketsConfig.length;
+  bracketsConfig.forEach(element => {
+    OPEN_BRACKETS.push(element[0]);
+    for (let i = 0; i <= k; i++) {
+      BRACKETS_PAIR[element[0]] = element[1];
+    }
+  });
+// console.log(OPEN_BRACKETS);
+// console.log(str.length);
+  let stack = [];
+   for(let i=0;i<str.length;i++){
+     let currentSymbol = str[i];
+   // console.log(currentSymbol);
+   //  console.log(BRACKETS_PAIR[currentSymbol]);
+      if (OPEN_BRACKETS.includes(currentSymbol)) {
+       stack.push(currentSymbol);
+     } 
+    //  else {
+    //    if (stack.length === 0) {
+    //      return false;
+    //    }
+      // console.log(stack);
+      //console.log(currentSymbol);
+      let topElement = stack[stack.length - 1];
+       // console.log(BRACKETS_PAIR[currentSymbol]);
+        if (BRACKETS_PAIR[currentSymbol] === topElement) {
+         stack.pop();
+         //console.log(stack);
+       }
+      // console.log(stack);
+ //  }
+}
+//console.log(stack);
+  // // your solution
 }
