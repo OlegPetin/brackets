f2ebd0be452461fb6bda27e6e00053e2b31f11dc
OlegPetin <olegpetin87@gmail.com>
2023-02-11 14:09:00 +0300

    feat: all test is correct

--- src/index.js
+++ src/index.js
@@ -1,38 +1,40 @@
 module.exports = function check(str, bracketsConfig) {
   const OPEN_BRACKETS = [];
+  const CLOSE_BRACKETS = [];
   const BRACKETS_PAIR = {};
   let k = bracketsConfig.length;
   bracketsConfig.forEach(element => {
     OPEN_BRACKETS.push(element[0]);
+    CLOSE_BRACKETS.push(element[1]);
     for (let i = 0; i <= k; i++) {
-      BRACKETS_PAIR[element[0]] = element[1];
+      BRACKETS_PAIR[element[1]] = element[0];
     }
   });
-// console.log(OPEN_BRACKETS);
-// console.log(str.length);
-  let stack = [];
+console.log(OPEN_BRACKETS);
+console.log(CLOSE_BRACKETS);
+console.log(BRACKETS_PAIR);
+const stack = [];
    for(let i=0;i<str.length;i++){
      let currentSymbol = str[i];
-   // console.log(currentSymbol);
-   //  console.log(BRACKETS_PAIR[currentSymbol]);
       if (OPEN_BRACKETS.includes(currentSymbol)) {
-       stack.push(currentSymbol);
+        if(CLOSE_BRACKETS.includes(currentSymbol) && stack[stack.length - 1] === currentSymbol){
+          stack.pop();
+        }else{
+          stack.push(currentSymbol);
+        }     
      } 
-    //  else {
-    //    if (stack.length === 0) {
-    //      return false;
-    //    }
-      // console.log(stack);
-      //console.log(currentSymbol);
-      let topElement = stack[stack.length - 1];
-       // console.log(BRACKETS_PAIR[currentSymbol]);
-        if (BRACKETS_PAIR[currentSymbol] === topElement) {
-         stack.pop();
-         //console.log(stack);
-       }
-      // console.log(stack);
- //  }
+     else {
+      if (stack.length === 0) {
+        return false;
+      } else {
+        if (BRACKETS_PAIR[currentSymbol] === stack[stack.length - 1]) {
+          stack.pop();
+        } else {
+         return false;
+        }
+            }
+          }
 }
-//console.log(stack);
-  // // your solution
+console.log(stack)
+return stack.length === 0;
 }
